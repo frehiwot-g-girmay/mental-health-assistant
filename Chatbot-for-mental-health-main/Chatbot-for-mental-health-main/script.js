666ebf50650b4610f9b481c77513cff36dad7c2e
Frehiwot Gebrekrstos Girmay <69894124+frehiwot-g-girmay@users.noreply.github.com>
2025-05-14 08:29:32 -0700

    Update script.js

--- Chatbot-for-mental-health-main/Chatbot-for-mental-health-main/script.js
+++ Chatbot-for-mental-health-main/Chatbot-for-mental-health-main/script.js
@@ -57,7 +57,7 @@ const generateResponse = (incomingChatLi) => {
 			messageElement
 			.classList.add("error");
 			messageElement
-			.textContent = "Oops! Something went wrong. Please try again!";
+			.textContent = "እዋእ! ገለ ነገር ተጋግዩ። በጃኹም ደጊምኩም ፈትኑ!";
 		})
 		.finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
 };
